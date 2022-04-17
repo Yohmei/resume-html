c6ad624a18a6bc1ddffc34e68c9b814d46cff57b
Max the Human <ayohmei@gmail.com>
2022-04-17 23:19:59 +0200

    nya

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCy-VIdxlh6di-FVWf9Frdf3hVa39EAvZQ',
+  authDomain: 'resume-html.firebaseapp.com',
+  projectId: 'resume-html',
+  storageBucket: 'resume-html.appspot.com',
+  messagingSenderId: '610611942248',
+  appId: '1:610611942248:web:e639904baa3988bd3c7cdd',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
